1dafdc51b8504c7c0a6b0ad8226430cb5d6afe8c
PrOrOk26 <vladgold2699@gmail.com>
2020-04-28 20:07:40 +0300

    MSA: Add promisification for the data router

--- server/routers/data.js
+++ server/routers/data.js
@@ -1,106 +1,118 @@
 const express = require("express");
 const dataRouter = express.Router();
 
-const { uri, collectionNames, dbName } = require("../constants.js");
+const { uri, dbName } = require("../constants.js");
 
 const MongoClient = require("mongodb").MongoClient;
-const client = new MongoClient(uri);
 
-let db = {};
+async function run() {
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+  }).catch((err) => console.error(err));
 
-client.connect(function (err) {
   console.log("Connected successfully to server");
-  db = client.db(dbName);
-});
+  const db = client.db(dbName);
 
-dataRouter.use(express.json({ type: "application/json" }));
+  dataRouter.use(express.json({ type: "application/json" }));
 
-dataRouter.get("/aids", function (req, res) {
-  const { by = "country" } = req.query;
-  let collectionName = "";
+  dataRouter.get("/aids", async function (req, res) {
+    const { by = "country" } = req.query;
+    let collectionName = "";
 
-  if (by === "region") {
-    collectionName = "aids_regions";
-  } else if (by === "country") {
-    collectionName = "aids_countries";
-  } else {
-    res.status(400).send("Bad request");
-    return;
-  }
-
-  const aidsCollection = db.collection(collectionName);
-
-  aidsCollection.find({}).toArray(function (err, data) {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
+    if (by === "region") {
+      collectionName = "aids_regions";
+    } else if (by === "country") {
+      collectionName = "aids_countries";
+    } else {
+      res.status(400).send("Bad request");
+      return;
     }
-    console.log(data);
+
+    const aidsCollection = db.collection(collectionName);
+
+    const data = await aidsCollection
+      .find({})
+      .toArray()
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      });
+
+    console.log('aidsData', data);
     res.json(data);
   });
-});
 
-dataRouter.get("/mortality/adult", function (req, res) {
-  const aidsCollection = db.collection("mortality_adult_countries");
+  dataRouter.get("/mortality/adult", async function (req, res) {
+    const collection = db.collection("mortality_adult_countries");
 
-  aidsCollection.find({}).toArray(function (err, data) {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
-    }
-    console.log(data);
+    const data = await collection
+      .find({})
+      .toArray()
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      });
+
+    console.log('mortalityData', data);
     res.json(data);
   });
-});
 
-dataRouter.get("/mortality/children", function (req, res) {
-  const { by = "country" } = req.query;
-  let collectionName = "";
+  dataRouter.get("/mortality/children", async function (req, res) {
+    const { by = "country" } = req.query;
+    let collectionName = "";
 
-  if (by === "region") {
-    collectionName = "mortality_children_regions";
-  } else if (by === "country") {
-    collectionName = "mortality_children_countries";
-  } else {
-    res.status(400).send("Bad request");
-    return;
-  }
-
-  const aidsCollection = db.collection(collectionName);
-
-  aidsCollection.find({}).toArray(function (err, data) {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
+    if (by === "region") {
+      collectionName = "mortality_children_regions";
+    } else if (by === "country") {
+      collectionName = "mortality_children_countries";
+    } else {
+      res.status(400).send("Bad request");
+      return;
     }
-    console.log(data);
+
+    const collection = db.collection(collectionName);
+
+    const data = await collection
+      .find({})
+      .toArray()
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      });
+
+    console.log('mortalityData', data);
     res.json(data);
+
   });
-});
 
-dataRouter.get("/life_expectancy", function (req, res) {
-  const { by = "region", type } = req.query;
-  let collectionName = "";
+  dataRouter.get("/life_expectancy", async function (req, res) {
+    const { by = "region", type } = req.query;
+    let collectionName = "";
 
-  if (by === "region" && type === "healthy") {
-    collectionName = "life_expectancy_birth_regions";
-  } else if (by === "region" && type !== "healthy") {
-    collectionName = "life_expectancy_60_regions";
-  } else {
-    res.status(400).send("Bad request");
-    return;
-  }
-
-  const aidsCollection = db.collection(collectionName);
-
-  aidsCollection.find({}).toArray(function (err, data) {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal server error");
+    if (by === "region" && type === "healthy") {
+      collectionName = "life_expectancy_birth_regions";
+    } else if (by === "region" && type !== "healthy") {
+      collectionName = "life_expectancy_60_regions";
+    } else {
+      res.status(400).send("Bad request");
+      return;
     }
-    console.log(data);
+    const collection = db.collection(collectionName);
+    
+    const data = await collection
+      .find({})
+      .toArray()
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      });
+
+    console.log('lifeExpectancyData', data);
     res.json(data);
+
   });
-});
+}
+
+run();
 
 module.exports = dataRouter;
